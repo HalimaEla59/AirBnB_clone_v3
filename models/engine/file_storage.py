ca201c4ee8da7999b3210b1c24190e3e7383211b
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2024-01-05 23:58:13 +0100

    task 2 file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -65,6 +65,21 @@ class FileStorage:
             if key in self.__objects:
                 del self.__objects[key]
 
+    def get(self, cls, id):
+        """retrieves one object, if not none"""
+        c_dict = self.all(cls)
+        id = cls + '.' + id
+        objct = c_dict.get(id)
+        return(objct)
+
+    def count(self, cls=None):
+        """counts the number of objects in storage matching the given class
+         If no class passed, returns the count of all objects in storage"""
+        counter = 0
+        c_dict = self.all(cls)
+        counter = len(c_dict)
+        return(counter)
+
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
